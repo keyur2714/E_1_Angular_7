242ff32f5f544bd50c843afa92e4d1750f663206
keyur2714 <keyur.555kn@gmail.com>
2019-04-20 09:36:40 +0530

    Added Reactive Forms With Form Array

--- TemplateDrivenFormsDemo/src/app/course-entry/course-entry.component.ts
+++ TemplateDrivenFormsDemo/src/app/course-entry/course-entry.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { Course } from '../inquiry-entry/course.model';
 
 @Component({
   selector: 'app-course-entry',
@@ -9,6 +10,8 @@ import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 export class CourseEntryComponent implements OnInit {
 
   courseEntryForm : FormGroup;
+  isCourseCreated : boolean = false;  
+  courseTrainerList : Course[] = [];
 
   constructor() { }
 
@@ -20,8 +23,8 @@ export class CourseEntryComponent implements OnInit {
     this.courseEntryForm = new FormGroup(
       {
         courseName : new FormControl('',Validators.required),
-        fees : new FormControl('',[Validators.required,Validators.pattern('^[0-9]+$')])
-        //trainerName : new FormArray([new FormControl()])
+        fees : new FormControl('',[Validators.required,Validators.pattern('^[0-9]+$')]),
+        trainersName : new FormArray([new FormControl()])
       }
     );
   }
@@ -31,5 +34,38 @@ export class CourseEntryComponent implements OnInit {
     console.log(this.courseEntryForm.value);
     console.log(this.courseEntryForm.get('courseName').value);
     console.log(this.courseEntryForm.get('fees').value);
+    console.log(this.courseEntryForm.get('trainersName').value);
+    if(this.courseEntryForm.valid){
+      this.isCourseCreated = true;
+      const courseName = this.courseEntryForm.get('courseName').value;
+      const fees = this.courseEntryForm.get('fees').value
+      for(let trainer of this.courseEntryForm.get('trainersName').value){
+        const newCourse = new Course();
+        newCourse.courseName = courseName;
+        newCourse.fees = fees;
+        newCourse.trainerName = trainer;
+        this.courseTrainerList.push(newCourse);
+      }
+    }
+  }
+
+  addNewTrainer():void{
+    let newTrainerControl = new FormControl();
+    (<FormArray>this.courseEntryForm.get("trainersName")).push(newTrainerControl); 
+  }
+
+  checkFeesValidation():void{
+    const courseName:string = this.courseEntryForm.get('courseName').value;
+    if(courseName.toUpperCase() === 'DEMO') {
+      this.courseEntryForm.get('fees').clearValidators();
+    }else{
+      this.courseEntryForm.get('fees').setValidators([Validators.required,Validators.pattern('^[0-9]+$')])      
+    }
+    this.courseEntryForm.get('fees').updateValueAndValidity;
+    this.courseEntryForm.updateValueAndValidity;
+  }
+  updateValidationMsg():void{
+    this.courseEntryForm.get('fees').clearValidators();
+    this.courseEntryForm.updateValueAndValidity;
   }
 }
