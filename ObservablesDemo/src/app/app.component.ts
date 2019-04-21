a6ddf844245f4d8e08911c1ddc93e7d27d6ebe7a
keyur2714 <keyur.555kn@gmail.com>
2019-04-21 14:00:37 +0530

    updated with pipe and tap

--- ObservablesDemo/src/app/app.component.ts
+++ ObservablesDemo/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable, interval, Subscription, Subject, from, of, range  } from 'rxjs';
-import { map, filter, scan } from 'rxjs/operators';
+import { Observable, interval, Subscription, Subject, from, of, range, pipe  } from 'rxjs';
+import { map, filter, scan, tap } from 'rxjs/operators';
 
 
 @Component({
@@ -18,62 +18,61 @@ export class AppComponent implements OnInit,OnDestroy{
   subject2 = new Subject<string>();
   nubmerObservables = interval(1000);
 
-  source$ = of(["keyur","denish","vinit","sachin","viru"]);
+  source$ = of("keyur","denish","vinit","ami","darsh","sachin","viru");
 
   ngOnInit(){
 
-     const filteredObservables:Observable<string[]> = this.source$.pipe(
-      filter( ( names : string[]) => {        
-         let i=0;
-         for(let name of names){
-           if(name.length > 4){
-            names[i]=name.toUpperCase();
-           }else{
-            names.splice(i,1);
-           }       
-           i++;   
-         }
-         if(names.length > 4){           
-            return true;
-         }
-      }),
-      map((names : string[]) => {  
-         let i=0;
-         for(let name of names){
-          names[i++]=name.toUpperCase();
-         }
-         return names;
-      })      
-    );
+      const calculationFun = pipe(
+        filter((n: number) => n % 2 === 1),
+        map((n: number) => n + 10)
+        //scan((sum, n) => sum + n)
+      );
     
-     filteredObservables.subscribe(
-       (data: string[])=>{
-         console.log("********"+ data);
+      //Instantiate response Observable
+      const calculation$ = calculationFun(of(1,2,3,4,5,6));
+    
+      //Subscribe the Observable instance
+      calculation$.subscribe(result => console.log(result));
+
+    //----------
+
+     const filteredFunc = pipe(
+      filter((name: string) => name.length > 4),
+      map((name : string) => name.toUpperCase())      
+     );
+     const result$ = filteredFunc(of("keyur","viru", "denish","vinit")); 
+     result$.subscribe(
+       (data)=>{
+         console.log(data);
        }
      )
     
-      this.source$.subscribe(
-        (data)=>{
-          console.log(data);
-        }
-      )
+     this.subscription = this.nubmerObservables.subscribe(
+       (data:number)=>{        
+         this.newNumber = data;
+       },(error)=>{
+       }
+     )
+     this.subject1.subscribe((data)=>{
+       console.log("Hi..."+ data);
+     });  
+     this.subject2.subscribe((data)=>{
+       console.log("Hello..."+ data);
+     });  
+      const observable1 = from([1, 2, 3]);    
+      observable1.subscribe(this.subject1);
+      const observable2 = of('Radhe Krishna...!');    
+      observable2.subscribe(this.subject2);
 
-    // this.subscription = this.nubmerObservables.subscribe(
-    //   (data:number)=>{        
-    //     this.newNumber = data;
-    //   },(error)=>{
-    //   }
-    // )
-    // this.subject1.subscribe((data)=>{
-    //   console.log("Hi..."+ data);
-    // });  
-    // this.subject2.subscribe((data)=>{
-    //   console.log("Hello..."+ data);
-    // });  
-    //  const observable1 = from([1, 2, 3]);    
-    //  observable1.subscribe(this.subject1);
-    //  const observable2 = of('Radhe Krishna...!');    
-    //  observable2.subscribe(this.subject2);
+      //Pipe and TAP
+
+      this.source$.pipe(
+        tap(  name => console.log("Process "+ name),
+              err => console.error(err),
+              () => console.log("Complete")
+        ),
+        filter(name => name.startsWith("d"))
+       ).subscribe(el => console.log("D names: "+ el)); 
   }
 
   ngOnDestroy(){
